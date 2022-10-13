838fe002af79049149997386a9bcad579f87d001
enddl3788 <enddl3788@naver.com>
2022-10-13 19:08:31 +0900

    first commit

---
+++ Tutorial-Book-Utils-main/PL_data_loader.py
@@ -0,0 +1,22 @@
+# taken from this repo: https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
+
+import gdown
+import argparse
+
+file_destinations = {'SoldierMark':'soldier mark.zip'}
+
+file_id_dic = {'SoldierMark':'1m0M4Fqt1SHygSqvBBJlQBJvNHNbZDrH4&export=download'}
+
+def download_file_from_google_drive(id_, destination):
+    url = f'https://drive.google.com/uc?id={id_}'
+    output = destination
+    gdown.download(url, output, quiet=False)
+    print(f'{output} download complete!')
+    
+parser = argparse.ArgumentParser(description = 'data loader for PseudoLab Tutorial Book')
+
+parser.add_argument('--data', type = str, help = 'key for selecting data')
+
+args = parser.parse_args()
+
+download_file_from_google_drive(id_=file_id_dic[args.data], destination=file_destinations[args.data])
